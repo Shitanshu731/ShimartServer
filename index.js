e6a25c596b551cf0deba6db5631e271493f08c3d
Shitcoder731 <shitanshu.731@gmail.com>
2025-02-15 02:44:59 +0530

    cors

--- index.js
+++ index.js
@@ -15,15 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [process.env.CLIENT_URL], // Allow only the frontend domain
-    credentials: true, // Allow credentials (cookies, auth headers)
-    methods: ["GET", "POST", "PUT", "DELETE"], // Explicitly allow methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Explicit headers
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(helmet());
