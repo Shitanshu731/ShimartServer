226c37b9f867b8d7f11cf6c3b2a7ed48f5cf6c74
Shitcoder731 <shitanshu.731@gmail.com>
2025-02-15 02:54:47 +0530

    cors

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(helmet());
